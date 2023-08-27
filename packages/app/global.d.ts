05235e737f3d10bf4314a310dda70af8916dd99e
dipanshuhappy <dipanshuhappy@gmail.com>
2023-08-27 19:08:42 +0530

    fix: added Ethereum global types

---
+++ packages/app/global.d.ts
@@ -0,0 +1,20 @@
+import { MetaMaskInpageProvider } from "@metamask/providers";
+type EthereumEvents = {
+  connect: { chainId: string };
+  disconnect: { message: string; code: number; data?: unknown };
+  accountsChanged: string[];
+  chainChanged: string;
+  close: unknown;
+};
+export interface Ethereum extends MetaMaskInpageProvider {
+  on<K extends keyof EthereumEvents>(
+    event: K,
+    handler: (event: EthereumEvents[K]) => void
+  ): void;
+  // other methods...
+}
+declare global {
+  interface Window {
+    ethereum: Ethereum;
+  }
+}
