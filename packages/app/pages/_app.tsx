51005f8585c44945c4dec98a1db55744c80777d5
Nithin-Varma <mnithin1422@gmail.com>
2023-09-08 00:50:09 +0530

    privy added

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -1,21 +1,23 @@
 import "../styles/globals.css";
 import "../styles/index.css";
 import "react-toastify/dist/ReactToastify.css";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import NextApp, { AppProps, AppContext } from 'next/app';
 import { getCookie, setCookie } from 'cookies-next';
 import Head from 'next/head';
 import { MantineProvider, ColorScheme, ColorSchemeProvider } from '@mantine/core';
 import { Web3ContextProvider } from "@/context/Web3Context";
-
+import {PrivyProvider} from '@privy-io/react-auth';
 
 import { WagmiConfig, configureChains, mainnet } from 'wagmi'
-import { publicProvider } from 'wagmi/providers/public'
 import wagmiConfig from "@/lib/wagmi";
+import { usePrivy } from '@privy-io/react-auth';
+
 
 
 
 export default function App(props: AppProps & { colorScheme: ColorScheme }) {
+  console.log(process.env.NEXT_PUBLIC_PRIVY_APP_ID)
   const { Component, pageProps } = props;
   const [colorScheme, setColorScheme] = useState<ColorScheme>(props.colorScheme);
 
@@ -24,6 +26,26 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
     setColorScheme(nextColorScheme);
     setCookie('mantine-color-scheme', nextColorScheme, { maxAge: 60 * 60 * 24 * 30 });
   };
+
+  const handleLogin = (user: any) => {
+    console.log(`User ${user.id} logged in!`)
+  }
+
+  // const {open} = usePrivy();
+  // const opening = open()
+
+  // // Call the open function inside a useEffect hook
+  // // to open the login popup when the component mounts
+  // useEffect(() => {
+  //   opening();
+  // }, [open]);
+
+
+  // useEffect(() => {
+  //   usePrivy()
+  // })
+
+
 
   return (
     <>
@@ -37,8 +59,22 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
         <WagmiConfig config={wagmiConfig}>
           <Web3ContextProvider>
             <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+            <PrivyProvider
+              appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ' ' }
+              onSuccess={handleLogin}
+              config={{
+                loginMethods: ['email', 'wallet'],
+                appearance: {
+                  theme: 'light',
+                  accentColor: '#676FFF',
+                    // This configures your login modal to show wallet login options above other options.
+                  showWalletLoginFirst: true,
+                  // logo: 'https://your-logo-url',
+                },
+              }}
+            >
               <Component {...pageProps} />
-
+              </PrivyProvider>
             </MantineProvider>
           </Web3ContextProvider>
         </WagmiConfig>
