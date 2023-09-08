5913fab26c84d0bc8f17bb93fbbd54e5e641df63
Nithin-Varma <mnithin1422@gmail.com>
2023-09-08 23:26:15 +0530

    filters setup

--- packages/app/components/PrizeComponents/filterComponent.tsx
+++ packages/app/components/PrizeComponents/filterComponent.tsx
@@ -2,25 +2,61 @@ import { useState } from 'react';
 import { MultiSelect } from '@mantine/core';
 
 const Filter = () => {
-  const [data, setData] = useState([
-    { value: '1', label: 'Proficiens' },
-    { value: '2', label: 'Priorities' },
+  const [proficiencydata, setProficiencyData] = useState([
+    { value: '1', label: 'Programming' },
+    { value: '2', label: 'Python' },
+    { value: '3', label: 'JavaScript' },
+    { value: '4', label: 'Writing' },
+    { value: '5', label: 'Design' },
+    { value: '6', label: 'Translation' },
+    { value: '7', label: 'Research' },
+    { value: '8', label: 'Real estate' },
+    { value: '9', label: 'Apps' },
+    { value: '10', label: 'Hardware' },
+    { value: '11', label: 'Art' },
+    { value: '12', label: 'Meta' },
+    { value: '13', label: 'AI' },
+  ]);
+
+  const [prioritiesdata, setPrioritiesData] = useState([
+    { value: '1', label: 'Climate Change' },
+    { value: '2', label: 'Network Civilizations' },
+    { value: '3', label: 'Open-Source' },
+    { value: '4', label: 'Community Coordination' },
+    { value: '5', label: 'Health' },
+    { value: '6', label: 'Education' },
   ]);
 
   return (
+    <>
     <MultiSelect
-      label="Select Which Suits to your Project"
-      data={data}
+      label="Proficiency"
+      maw={"300px"}
+      data={proficiencydata}
       placeholder="Select items"
       searchable
       creatable
       getCreateLabel={(query) => `+ Create ${query}`}
       onCreate={(query) => {
         const item = { value: query, label: query };
-        setData((current) => [...current, item]);
+        setProficiencyData((current) => [...current, item]);
         return item;
       }}
     />
+    <MultiSelect
+      maw={"300px"}
+      label="Priorities"
+      data={prioritiesdata}
+      placeholder="Select items"
+      searchable
+      creatable
+      getCreateLabel={(query) => `+ Create ${query}`}
+      onCreate={(query) => {
+        const item = { value: query, label: query };
+        setPrioritiesData((current) => [...current, item]);
+        return item;
+      }}
+    /></>
   );
 }
 
