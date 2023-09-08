02cc1e1bfeec19523669abfbbee4bd1ac093460a
swaraj bachu <rajswaraj.r@gmail.com>
2023-09-09 01:45:14 +0530

    editor

---
+++ packages/app/pages/prize/editor/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Editor } from "novel";
+
+
+function EditorsPage() {
+  return (
+    <div className='w-full grid place-content-center my-3'>
+        <Editor />
+    </div>
+  )
+}
+
+export default EditorsPage
