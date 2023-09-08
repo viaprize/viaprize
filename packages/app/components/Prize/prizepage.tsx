a066e56a424d38a6837ceeadefd288442a173fae
swaraj bachu <rajswaraj.r@gmail.com>
2023-09-08 22:42:59 +0530

    dropzone

---
+++ packages/app/components/Prize/prizepage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ImageComponent from './dropzone'
+
+export default function PrizePageComponent() {
+  return (
+    <div className='max-w-screen-xl'>
+      <ImageComponent />
+    </div>
+  )
+}
