a6fb89121adadf2bff50f600c28617fc9b0a940a
Aryan Tiwari <aryantiwari7762@gmail.com>
2023-09-09 18:00:43 +0530

    header

--- packages/app/components/layout/headerLayout.tsx
+++ packages/app/components/layout/headerLayout.tsx
@@ -1,14 +1,22 @@
+import React from "react";
+import { Avatar, Popover, Button, Paper ,Text} from '@mantine/core';
 import Link from "next/link";
 
 export default function HeaderLayout  ()  {
   return (
-    <div className="bg-black h-[70px] w-full text-white">
-      <Link href="/prize/explorePrize">
-        explore prizes
-      </Link>
-      <Link href="/prize/123">
-        just switching
-      </Link>
+    <div className="bg-black h-[70px]">
+      <div className="absolute right-0 m-4">
+      <Popover width={200} position="bottom" withArrow shadow="md" >
+      <Popover.Target >
+      <Avatar />
+      </Popover.Target>
+      <Popover.Dropdown>
+        <Link href="/profile">
+     <Text>view Profile</Text>
+        </Link>
+      </Popover.Dropdown>
+    </Popover>
+    </div>
     </div>
   )
 }
