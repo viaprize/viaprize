2a41b2af66e0be4fdcbbfbcbc5bcb7de67b8d315
swaraj bachu <rajswaraj.r@gmail.com>
2023-09-09 23:45:56 +0530

    added header

--- packages/app/components/layout/headerLayout.tsx
+++ packages/app/components/layout/headerLayout.tsx
@@ -1,13 +1,32 @@
 import React, { forwardRef } from 'react';
-import { Avatar, Popover, Button, Paper, Text, Group, Menu, UnstyledButton } from '@mantine/core';
+import {
+  Avatar,
+  Popover,
+  Button,
+  Paper,
+  Text,
+  Group,
+  Menu,
+  UnstyledButton,
+  Badge,
+} from '@mantine/core';
 import Link from 'next/link';
 import { usePrivy } from '@privy-io/react-auth';
-import { IconArrowsLeftRight, IconChevronRight, IconMessageCircle, IconPhoto, IconSearch, IconSettings, IconTrash, IconUser } from '@tabler/icons-react';
+import {
+  IconArrowsLeftRight,
+  IconChevronRight,
+  IconMessageCircle,
+  IconPhoto,
+  IconSearch,
+  IconSettings,
+  IconTrash,
+  IconUser,
+} from '@tabler/icons-react';
 import { IoMdContact } from 'react-icons/io';
+import { toast } from 'sonner';
+import { useRouter } from 'next/router';
 
 export default function HeaderLayout() {
-  const { logout } = usePrivy();
-
   return (
     <div className="bg-black h-[70px]">
       <div className="absolute right-0 m-4">
@@ -31,23 +50,55 @@ export default function HeaderLayout() {
 }
 
 function ProfileMenu() {
+  const { logout, user } = usePrivy();
+  const router = useRouter();
+
+  const handleLogout = () => {
+    try {
+      toast.promise(logout(), {
+        loading: 'Logging out',
+        success: 'Logged out',
+        error: 'Error logging out',
+      });
+    } catch {
+      toast.error('Error logging out');
+    }
+  };
   return (
     <Group position="center">
-      <Menu withArrow>
+      <Menu withArrow trigger="hover" openDelay={100} closeDelay={400}>
         <Menu.Target>
-          <Avatar color="cyan" radius="xl" className="cursor-pointer">
-            MK
-          </Avatar>
+          {user ? (
+            <Badge
+              color="dark"
+              variant="filled"
+              size="xl"
+              className="absolute top-0 right-0 cursor-pointer"
+            >
+              {user?.wallet?.address?.slice(0, 4) + '...' + user?.wallet?.address?.slice(-4)}
+            </Badge>
+          ) : (
+            <Avatar color="cyan" radius="xl" className="cursor-pointer" />
+          )}
         </Menu.Target>
         <Menu.Dropdown p="md" mr="sm">
           <Menu.Label>Profile</Menu.Label>
-          <Menu.Item icon={<IconUser size={14} />}>View Profile</Menu.Item>
+          {user ? (
+            <Menu.Item
+              icon={<IconUser size={14} />}
+              onClick={() => {
+                router.push('/profile');
+              }}
+            >
+              View Profile
+            </Menu.Item>
+          ) : null}
           {/* <Menu.Item icon={<IconMessageCircle size={14} />}>Messages</Menu.Item>
           <Menu.Item icon={<IconPhoto size={14} />}>Gallery</Menu.Item> */}
           <Menu.Item
             icon={<IconSearch size={14} />}
             rightSection={
-              <Text size="xs" color="dimmed">
+              <Text size="xs" color="dimmed" pl="sm">
                 ⌘K
               </Text>
             }
@@ -57,11 +108,23 @@ function ProfileMenu() {
 
           <Menu.Divider />
 
-          <Menu.Label>Danger zone</Menu.Label>
+          {/* <Menu.Label>Danger zone</Menu.Label> */}
           {/* <Menu.Item icon={<IconArrowsLeftRight size={14} />}>Transfer my data</Menu.Item> */}
-          <Menu.Item color="red" icon={<IconTrash size={14} />}>
-            Logout
-          </Menu.Item>
+          {user ? (
+            <Menu.Item color="red" icon={<IconTrash size={14} />} onClick={handleLogout}>
+              Logout
+            </Menu.Item>
+          ) : (
+            <Menu.Item
+              color="green"
+              icon={<IconUser size={14} />}
+              onClick={() => {
+                router.push('/');
+              }}
+            >
+              Login
+            </Menu.Item>
+          )}
         </Menu.Dropdown>
       </Menu>
     </Group>
