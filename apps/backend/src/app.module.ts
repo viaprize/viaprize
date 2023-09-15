6f71ae9f94d087f6e85086ecf035424c9f9bc5f1
swaraj bachu <rajswaraj.r@gmail.com>
2023-09-15 18:46:17 +0530

    init

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -19,7 +19,7 @@ import { MailModule } from 'src/mail/mail.module';
 import { MailerModule } from 'src/mailer/mailer.module';
 import { JobsModule } from './jobs/jobs.module';
 import mailConfig from './config/mail.config';
-import { EthersModule } from 'nestjs-ethers';
+// import { EthersModule } from 'nestjs-ethers';
 
 @Module({
   imports: [
