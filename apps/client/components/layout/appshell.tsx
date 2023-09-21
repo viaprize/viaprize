8a88e9d3c699b5fb8d281556e424e92a8c06f2fd
swaraj bachu <rajswaraj.r@gmail.com>
2023-09-21 21:21:43 +0530

    some settings

--- apps/client/components/layout/appshell.tsx
+++ apps/client/components/layout/appshell.tsx
@@ -1,17 +1,13 @@
-import { useState } from "react";
 import {
   AppShell,
-  Navbar,
-  Header,
-  Footer,
-  Aside,
-  Text,
-  MediaQuery,
   Burger,
-  useMantineTheme,
   Center,
+  Header,
+  MediaQuery,
+  useMantineTheme,
 } from "@mantine/core";
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
+import { useState } from "react";
 import HeaderLayout from "./headerLayout";
 
 export default function AppShellLayout({ children }: { children: ReactNode }) {
@@ -19,15 +15,6 @@ export default function AppShellLayout({ children }: { children: ReactNode }) {
   const [opened, setOpened] = useState(false);
   return (
     <AppShell
-      styles={{
-        main: {
-          background:
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[8]
-              : theme.colors.gray[0],
-        },
-      }}
-      navbarOffsetBreakpoint="sm"
       asideOffsetBreakpoint="sm"
       header={
         <Header height={{ base: 50, md: 70 }} p="md">
@@ -36,17 +23,28 @@ export default function AppShellLayout({ children }: { children: ReactNode }) {
           >
             <MediaQuery largerThan="sm" styles={{ display: "none" }}>
               <Burger
-                opened={opened}
-                onClick={() => setOpened((o) => !o)}
-                size="sm"
                 color={theme.colors.gray[6]}
                 mr="xl"
+                onClick={() => {
+                  setOpened((o) => !o);
+                }}
+                opened={opened}
+                size="sm"
               />
             </MediaQuery>
             <HeaderLayout />
           </div>
         </Header>
       }
+      navbarOffsetBreakpoint="sm"
+      styles={{
+        main: {
+          background:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[8]
+              : theme.colors.gray[0],
+        },
+      }}
     >
       <Center>{children}</Center>
     </AppShell>
