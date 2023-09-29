cdb2ccbc4ac3af7d835b5a87652bc11b7d241bd6
dipanshuhappy <dipanshuhappy@gmail.com>
2023-09-29 14:21:04 +0530

    fix: build issues

--- backend/src/pacts/pacts.module.ts
+++ backend/src/pacts/pacts.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MailModule } from 'src/mail/mail.module';
-import { PactEntity } from './entities/pact.entity';
+import { Pact } from './entities/pact.entity';
 import { PactsController } from './pacts.controller';
 import { PactsService } from './pacts.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PactEntity]), MailModule],
+  imports: [TypeOrmModule.forFeature([Pact]), MailModule],
   controllers: [PactsController],
   providers: [PactsService],
   exports: [PactsService],
 })
-export class PactsModule {}
+export class PactsModule { }
--- backend/src/pacts/pacts.service.ts
+++ backend/src/pacts/pacts.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { CreatePact } from './dto/create-pact.dto';
-import { PactEntity } from './entities/pact.entity';
-import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MailService } from 'src/mail/mail.service';
+import { Repository } from 'typeorm';
+import { CreatePact } from './dto/create-pact.dto';
+import { Pact } from './entities/pact.entity';
 /* The PactsService class is responsible for creating, finding, and retrieving pact entities, and it
 also uses the MailService to perform a test operation. */
 @Injectable()
 export class PactsService {
   constructor(
-    @InjectRepository(PactEntity)
-    private pactRepository: Repository<PactEntity>,
+    @InjectRepository(Pact)
+    private pactRepository: Repository<Pact>,
     private mailService: MailService,
-  ) {}
+  ) { }
   async create(createPact: CreatePact) {
     const pact = this.pactRepository.create(createPact);
     await this.pactRepository.insert(pact);
