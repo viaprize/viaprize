4fa5895331937436cde4c4515cb36266c16d2854
swaraj bachu <rajswaraj.r@gmail.com>
2023-10-03 16:43:20 +0700

    sorting with url state

--- apps/client/components/ExplorePrize/searchFilters.tsx
+++ apps/client/components/ExplorePrize/searchFilters.tsx
@@ -1,11 +1,40 @@
-import { Button, Drawer, Group, Text, TextInput } from "@mantine/core";
+import { Button, Drawer, Group, Menu, Text, TextInput } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { IconSearch } from "@tabler/icons-react";
+import { useRouter } from "next/router";
 import Filter from "./filterComponent";
+
 // import Filter from "./filterComponent";
+
+type Sorts = Record<string, string>;
+
+const sorts: Sorts = {
+  "Date: Old to New": "date.asc",
+  "Date: New to Old": "date.desc",
+  "Prize: Low to High": "prize.asc",
+  "Prize: High to Low": "prize.desc",
+  "Deadline: Sooner to Later": "deadline.asc",
+  "Deadline: Later to Sooner": "deadline.desc",
+};
+
+const sortKeys = Object.keys(sorts).map((key) => ({
+  label: key,
+  value: sorts[key],
+}));
 
 export function SearchFilters() {
   const [opened, { open, close }] = useDisclosure(false);
+
+  const router = useRouter();
+  const searchParams = router.query;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any -- TODO: fix
+  const params = new URLSearchParams(searchParams as any as string);
+
+  const handleSort = async (value: string) => {
+    params.set("sort", value);
+    await router.push({ query: params.toString() });
+  };
+
   return (
     <div className="p-5">
       <Text size="25px" weight={500}>
@@ -22,7 +51,27 @@ export function SearchFilters() {
         />
         <Group position="right">
           <Button onClick={open}>Filter</Button>
-          <Button>Sort</Button>
+          {/* <Button>Sort</Button> */}
+          <Menu shadow="md" width={200}>
+            <Menu.Target>
+              <Button>Sort</Button>
+            </Menu.Target>
+
+            <Menu.Dropdown>
+              <Menu.Label>Sort By</Menu.Label>
+              <Menu.Divider />
+              {sortKeys.map((key) => {
+                return (
+                  <Menu.Item
+                    key={key.value}
+                    onClick={() => void handleSort(key.value)}
+                  >
+                    {key.label}
+                  </Menu.Item>
+                );
+              })}
+            </Menu.Dropdown>
+          </Menu>
         </Group>
       </Group>
       <Drawer opened={opened} onClose={close} title="Filters" position="right">
