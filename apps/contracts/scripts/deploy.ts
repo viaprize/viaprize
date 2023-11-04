7241a159507de1c6e8f2180b73716c477c9c6360
Nithin-Varma <mnithin1422@gmail.com>
2023-11-04 14:49:06 +0530

    deploy script

--- apps/contracts/scripts/deploy.ts
+++ apps/contracts/scripts/deploy.ts
@@ -5,17 +5,23 @@ async function main() {
 
   console.log("Deploying contracts with the account:", deployer.address);
 
-  const submissionTree = await ethers.getContractFactory("SubmissionAVLTree");
+  const submissionTree = await ethers.getContractFactory("SubmissionLibrary");
   const submission_tree = await submissionTree.deploy();
   await submission_tree.deployed();
 
   console.log("SubmissionAVLTree Contract Address:", submission_tree.address);
 
-  const viaPrize = await ethers.getContractFactory("ViaPrize");
+  const viaPrize = await ethers.getContractFactory("ViaPrize", {
+    libraries: {
+      SubmissionLibrary: submission_tree.address,
+    }
+  });
   const viaPrize_contract = await viaPrize.deploy(
-    submission_tree.address,
+    ["0x5B38Da6a701c568545dCfcB03FcB875f56beddC4", "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2"],
+    ["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2", "0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB"],
     10,
     10,
+    "0x17F6AD8Ef982297579C203069C1DbfFE4348c372",
   );
   await viaPrize_contract.deployed();
 
