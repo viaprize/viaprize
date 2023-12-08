cd2190143aaf6b4520048f8c7c38e354552a81da
swaraj bachu <rajswaraj.r@gmail.com>
2023-12-09 01:29:22 +0530

    suspense

--- apps/client/app/(dashboard)/prize/explore/page.tsx
+++ apps/client/app/(dashboard)/prize/explore/page.tsx
@@ -1,35 +1,37 @@
 import ExploreCard from '@/components/ExplorePrize/explorePrize';
 import SearchFilters from '@/components/ExplorePrize/searchFilters';
-import { formatEther } from 'viem';
 import { Api } from '@/lib/api';
+import { Suspense } from 'react';
+import { formatEther } from 'viem';
 
 async function ExplorePage() {
+  const prizes = (
+    await new Api().prizes.prizesList({
+      limit: 10,
+      page: 1,
+    })
+  ).data.data;
 
- const prizes = (
-   await new Api().prizes.prizesList({
-     limit: 10,
-     page: 1,
-   })
- ).data.data;
- 
   return (
     <div className="max-w-screen-xl">
       <SearchFilters />
       <div className="p-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3  gap-4">
-        {prizes.map((prize) => {
-          return (
-            <ExploreCard
-              description={prize.description}
-              imageUrl={prize.images[0]}
-              deadline=""
-              money={formatEther(BigInt(prize.balance))}
-              profileName={prize.user ? prize.user.name : ''}
-              title={prize.title}
-              key={prize.id}
-              id={prize.id}
-            />
-          );
-        })}
+        <Suspense fallback={<div>Loading...</div>}>
+          {prizes.map((prize) => {
+            return (
+              <ExploreCard
+                description={prize.description}
+                imageUrl={prize.images[0]}
+                deadline=""
+                money={formatEther(BigInt(prize.balance))}
+                profileName={prize.user ? prize.user.name : ''}
+                title={prize.title}
+                key={prize.id}
+                id={prize.id}
+              />
+            );
+          })}
+        </Suspense>
         {/* Add as many ExploreCard components as you need */}
       </div>
     </div>
@@ -37,5 +39,3 @@ async function ExplorePage() {
 }
 
 export default ExplorePage;
-
-
