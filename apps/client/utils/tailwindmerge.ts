edabe5d0f623a70c04496036459a5cf136aedabb
swaraj bachu <rajswaraj.r@gmail.com>
2023-12-09 18:48:41 +0530

    login to view profile

--- apps/client/utils/tailwindmerge.ts
+++ apps/client/utils/tailwindmerge.ts
@@ -1,4 +1,7 @@
-import {twMerge} from 'tailwind-merge'
-import {clsx} from 'clsx'
+import type { ClassValue } from 'clsx';
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
-export const cn = twMerge(clsx())
+export function cn(
+    ...inputs: ClassValue[]
+) { return twMerge(clsx(inputs)) }
