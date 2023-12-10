8b5400cd3967b7665115e6c3f6cd1895f9da2269
Aryan Tiwari <aryantiwari7762@gmail.com>
2023-12-10 08:01:46 +0530

    ui fixes

--- apps/client/app/(dashboard)/portal/[id]/page.tsx
+++ apps/client/app/(dashboard)/portal/[id]/page.tsx
@@ -4,12 +4,11 @@ import PortalTabs from './portal-tabs';
 
 export default function CreatePortal() {
   return (
-    <div className="my-10">
-      <div className="w-full lg:flex gap-4 px-3 sm:px-6 md:px-14 lg:px-20 justify-between">
+    <div className="my-10 px-3 sm:px-6 md:px-14 lg:px-20">
+      <div className="w-full lg:flex gap-4 justify-between">
         <ImageTitleHeroCard />
         <AmountDonateCard />
       </div>
-
       <PortalTabs />
     </div>
   );
