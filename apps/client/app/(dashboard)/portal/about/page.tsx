5d50d77f235dcc2cef8b5416c093459a7348c3f8
swaraj bachu <rajswaraj.r@gmail.com>
2023-12-10 08:25:28 +0530

    added portal about page

---
+++ apps/client/app/(dashboard)/portal/about/page.tsx
@@ -0,0 +1,32 @@
+import { Card } from '@mantine/core';
+import PortalAboutHero from './hero';
+
+export default function PortalAbout() {
+  return (
+    <div className='my-10'>
+      <PortalAboutHero />
+      <div className="grid grid-cols-2 gap-2">
+        <Card withBorder shadow="md" radius="md">
+          <h1>GoFundMe style (pass-through campaigns)</h1>
+          <p>
+            If the author chooses to immediately forward contributions to the recipient,
+            then it functions just like GoFundMe. It is not required, but the author may
+            choose to add a funding goal and a deadline for when the campaign will
+            automatically end. In this style, the funding goal and the deadline are
+            editable. The campaign only ends if the author ends it manually.
+          </p>
+        </Card>
+        <Card withBorder shadow="md" radius="md">
+          <h1>GoFundMe style (pass-through campaigns)</h1>
+          <p>
+            If the author chooses to immediately forward contributions to the recipient,
+            then it functions just like GoFundMe. It is not required, but the author may
+            choose to add a funding goal and a deadline for when the campaign will
+            automatically end. In this style, the funding goal and the deadline are
+            editable. The campaign only ends if the author ends it manually.
+          </p>
+        </Card>
+      </div>
+    </div>
+  );
+}
