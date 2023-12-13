ed55ec152d3b939655fa8fe31358969d8643327e
swaraj bachu <rajswaraj.r@gmail.com>
2023-12-13 16:26:11 +0530

    some basic image changes needed on home

--- apps/client/app/(dashboard)/portal/about/page.tsx
+++ apps/client/app/(dashboard)/portal/about/page.tsx
@@ -1,5 +1,4 @@
 import { Card } from '@mantine/core';
-import React from 'react';
 import PortalAboutHero from './hero';
 
 export default function PortalAbout() {
@@ -11,10 +10,11 @@ export default function PortalAbout() {
           <h1>GoFundMe style (pass-through)</h1>
           <p>
             If the author chooses to immediately forward contributions to the recipient,
-            then it functions just like GoFundMe. It is not required, but the author may
-            choose to add a funding goal and a deadline for when the campaign will
-            automatically end. In this style, the funding goal and the deadline are
-            editable. The campaign only ends if the author ends it manually.
+            then it functions just like GoFundMe. There is a 5% platform fee. It is not
+            required, but the author may choose to add a funding goal and a deadline for
+            when the campaign will automatically end. In this style, the funding goal and
+            the deadline are editable. The campaign only ends if the author ends it
+            manually.
           </p>
         </Card>
         <Card withBorder shadow="md" radius="md">
@@ -24,11 +24,11 @@ export default function PortalAbout() {
             a deadline and a goal. This is similar to campaigns on Kickstarter. The author
             must also choose whether to disburse funds as soon as the funding goal is met
             or to allow contributions above the goal that are all disbursed at the
-            deadline. If the funding goal is not met by the deadline, then all the
-            contributions are automatically refunded. In this style, the funding goal and
-            deadline are immutable. The author may cancel the campaign and refund
-            everyone. Otherwise the campaign ends automatically either when the goal is
-            met or the deadline is reached.
+            deadline minus a 5% platform fee. If the funding goal is not met by the
+            deadline, then all the contributions are automatically refunded and there are
+            no fees. In this style, the funding goal and deadline are immutable. The
+            author may cancel the campaign and refund everyone. Otherwise the campaign
+            ends automatically either when the goal is met or the deadline is reached.
           </p>
         </Card>
       </div>
