4871d1a3ebcce08a983a8c0943bcf30743a79b3e
dipanshuhappy <dipanshuhappy@gmail.com>
2023-12-13 11:37:58 +0530

    total funds

--- apps/client/app/(dashboard)/portal/[id]/page.tsx
+++ apps/client/app/(dashboard)/portal/[id]/page.tsx
@@ -1,12 +1,12 @@
 import { Api } from '@/lib/api';
+import { formatEther } from 'viem';
 import AmountDonateCard from './amount-donate-card';
 import ImageTitleHeroCard from './image-title-hero-card';
 import PortalTabs from './portal-tabs';
-import { formatEther } from 'viem';
 
 export default async function CreatePortal({ params }: { params: { id: string } }) {
   const portal = (await new Api().portals.portalsDetail(params.id)).data;
-  console.log(portal)
+  console.log(portal);
   return (
     <div className="my-10 px-3 sm:px-6 md:px-14 lg:px-20">
       <div className="w-full lg:flex gap-4 justify-between">
@@ -16,7 +16,7 @@ export default async function CreatePortal({ params }: { params: { id: string } 
           img={portal.images[0]}
         />
         <AmountDonateCard
-          amountRaised={formatEther(BigInt(portal.balance))}
+          amountRaised={formatEther(BigInt(portal.totalFunds ?? 0))}
           recipientAddress={portal.contract_address}
           totalContributors={formatEther(BigInt(portal.totalFunds ?? 0))}
           contractAddress={portal.contract_address}
