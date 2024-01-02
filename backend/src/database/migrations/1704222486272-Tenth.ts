7b8647a556ec28219dc8d87a72375283bd8b1757
dipanshuhappy <dipanshuhappy@gmail.com>
2024-01-03 00:39:57 +0530

    Update branch name to main and delete unused migration file

---
+++ backend/src/database/migrations/1704222486272-Tenth.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Tenth1704222486272 implements MigrationInterface {
+    name = 'Tenth1704222486272'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "prize_proposals" ADD "proposerFeePercentage" integer NOT NULL DEFAULT '5'`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "prize_proposals" DROP COLUMN "proposerFeePercentage"`);
+    }
+
+}
