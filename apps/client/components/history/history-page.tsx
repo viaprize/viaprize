fbc06e637397274b9108032b72063edd2c6bcc69
Aryan Tiwari <aryantiwari7762@gmail.com>
2024-03-06 18:36:47 +0530

    done all history fetching and display

--- apps/client/components/history/history-page.tsx
+++ apps/client/components/history/history-page.tsx
@@ -9,16 +9,19 @@ export default async function HistoryPage() {
       <div className="grid grid-cols-3 gap-4">
         {data.map((prize) => {
           if (
-            prize.Awarded &&
-            prize.PrizeName &&
-            prize.DatePosted &&
-            prize.AwardedUSDe &&
-            prize.WinnersAmount
+            (prize.Awarded &&
+              // prize.WonRefunded &&
+              prize.PrizeName) ||
+            prize.DatePosted ||
+            prize.AwardedUSDe ||
+            prize.WinnersAmount // Changed this line
           ) {
+            const status = prize.WinnersAmount ? 'Won' : 'Not Won'; // Adjusted status based on WinnersAmount existence
             return (
               <HistoryCard
+                imageUrl={prize.Image}
                 key={prize.PrizeName}
-                status="Won"
+                status={status} // Passed the adjusted status here
                 datePosted={prize.DatePosted}
                 title={prize.PrizeName}
                 description={prize.WinnersAmount}
