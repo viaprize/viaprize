deed9938ddeb5bac6b67e5686008c066160ae9e1
swaraj bachu <rajswaraj.r@gmail.com>
2024-03-07 18:40:19 +0530

    migration

--- backend/src/portals/entities/portals-comments.entity.ts
+++ backend/src/portals/entities/portals-comments.entity.ts
@@ -1,19 +1,41 @@
-import { User } from "src/users/entities/user.entity";
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
-import { Portals } from "./portal.entity";
+import { User } from 'src/users/entities/user.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Portals } from './portal.entity';
 
 @Entity()
 export class PortalsComments {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    comment: string;
+  @Column()
+  comment: string;
 
-    @ManyToOne(() => User, (user) => user.portalComments)
-    @JoinColumn({ name: 'user', referencedColumnName: 'authId' })
-    user: User;
+  // Define self-referencing parent-child relationship
+  @ManyToOne(
+    () => PortalsComments,
+    (parentComment) => parentComment.childComments,
+    { nullable: true },
+  )
+  @JoinColumn({ name: 'parent_comment_id' })
+  parentComment: PortalsComments;
 
-    @ManyToOne(() => Portals, (portals) => portals.comments)
-    portal: Portals;
+  @OneToMany(
+    () => PortalsComments,
+    (childComment) => childComment.parentComment,
+  )
+  childComments: PortalsComments[];
+
+  @ManyToOne(() => User, (user) => user.portalComments)
+  @JoinColumn({ name: 'user', referencedColumnName: 'authId' })
+  user: User;
+
+  @ManyToOne(() => Portals, (portals) => portals.comments)
+  portal: Portals;
 }
--- backend/src/portals/services/portal-comments.service.ts
+++ backend/src/portals/services/portal-comments.service.ts
@@ -5,39 +5,37 @@ import { Repository } from 'typeorm';
 import { PortalsComments } from '../entities/portals-comments.entity';
 import { PortalsService } from './portals.service';
 
-
 @Injectable()
 export class PortalCommentService {
-    constructor(
-        @InjectRepository(PortalsComments)
-        private portalCommentsRepository: Repository<PortalsComments>,
-        private userService: UsersService,
-        private portalService: PortalsService
-    ) { }
+  constructor(
+    @InjectRepository(PortalsComments)
+    private portalCommentsRepository: Repository<PortalsComments>,
+    private userService: UsersService,
+    private portalService: PortalsService,
+  ) {}
 
-    async create(comment: string, userAuthId: string, portalId: string) {
-        const user = await this.userService.findOneByAuthId(userAuthId);
+  async create(comment: string, userAuthId: string, portalId: string) {
+    const user = await this.userService.findOneByAuthId(userAuthId);
 
-        const portal = await this.portalService.findAndGetByIdOnly(portalId);
+    const portal = await this.portalService.findAndGetByIdOnly(portalId);
 
-        const portalComment = await this.portalCommentsRepository.save({
-            comment: comment,
-            user: user,
-            portal: portal
-        });
-        return portalComment;
-    }
+    const portalComment = await this.portalCommentsRepository.save({
+      comment: comment,
+      user: user,
+      portal: portal,
+    });
+    return portalComment;
+  }
 
-    async getCommentsByPortalId(portalId: string) {
-        const portalComments = await this.portalCommentsRepository.find({
-            where: {
-                portal: {
-                    id: portalId
-                }
-            },
-            relations: ['user'],
-        });
-        return portalComments;
-    }
-
+  async getCommentsByPortalId(portalId: string) {
+    const portalComments = await this.portalCommentsRepository.find({
+      where: {
+        portal: {
+          id: portalId,
+        },
+      },
+      relations: ['user'],
+    });
+    return portalComments;
+  }
 }
