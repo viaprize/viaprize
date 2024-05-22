b64a504d7a0fe00fc8e4579f4a41509e2966008a
swaraj bachu <rajswaraj.r@gmail.com>
2024-05-22 11:30:54 +0300

    save point

--- apps/client/pages/submission/[id]/edit.tsx
+++ apps/client/pages/submission/[id]/edit.tsx
@@ -1,24 +1,41 @@
 import type { Submission } from '@/lib/api';
 import { Api } from '@/lib/api';
-import { Paper } from '@mantine/core';
+import { Button, Paper } from '@mantine/core';
 import type { JSONContent } from '@tiptap/react';
 import type { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { Editor as NovalEditor } from 'novel';
+import { useState } from 'react';
 
 export default function SubmissionPage({
   submission,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   console.log(submission);
+  const [content, setContent] = useState<JSONContent | undefined>(
+    JSON.parse(submission.submissionDescription) as JSONContent,
+  );
+  const startSubmissionDate = new Date(submission.prize.startSubmissionDate);
+
+  // Calculate the submission deadline by adding the submission days
+  const submissionDeadline = new Date(startSubmissionDate);
+  submissionDeadline.setDate(
+    submissionDeadline.getDate() + submission.prize.submissionTime,
+  );
+
+  if (new Date() > submissionDeadline) {
+    <Paper>You Cant Edit this Submission</Paper>;
+  }
+
   return (
     <Paper>
       <NovalEditor
         className=""
         disableLocalStorage
-        editorProps={{
-          editable: () => false,
+        defaultValue={content}
+        onUpdate={(newContent) => {
+          setContent(newContent);
         }}
-        defaultValue={JSON.parse(submission.submissionDescription) as JSONContent}
       />
+      <Button>Update Submission</Button>
     </Paper>
   );
 }
