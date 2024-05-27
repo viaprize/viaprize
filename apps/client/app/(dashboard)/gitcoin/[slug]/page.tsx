ce1df235cc909fa2d2dc7c90e36b215635ecfc5a
swaraj bachu <rajswaraj.r@gmail.com>
2024-05-27 18:11:34 +0300

    some changes

--- apps/client/app/(dashboard)/gitcoin/[slug]/page.tsx
+++ apps/client/app/(dashboard)/gitcoin/[slug]/page.tsx
@@ -20,8 +20,8 @@ export default async function GitcoinApplication({
   return (
     <div className="my-10 px-3 sm:px-6 md:px-14 lg:px-20">
       <ImageTitleCard
-        title="SOLARPUNK GUILD, gamifying hypercerts for Guild Members impact assessment "
-        name="by 0xEB7...7B9D0"
+        title={applicationsInRound.applicationByNodeId.project.name}
+        name={applicationsInRound.applicationByNodeId.project.name}
         img="https://d16c97c2np8a2o.cloudfront.net/ipfs/bafkreigf6g54vvjcbqogij5dri5iauvqbmhbpsgoasvm5scapjpp56k6ji?img-height=640"
       />
       <div className="w-full lg:flex gap-4 justify-between mt-3">
