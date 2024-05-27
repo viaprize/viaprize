dd7bdddce07620fd7bc597fd0b642d444fb514e6
swaraj bachu <rajswaraj.r@gmail.com>
2024-05-27 11:01:36 +0300

    yooo, finally made the dynamic graphql query work

--- apps/client/lib/actions.ts
+++ apps/client/lib/actions.ts
@@ -1,14 +1,47 @@
-// lib/fetchRoundByNodeId.ts
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+'use server';
+
+import { ApolloClient, InMemoryCache, gql, ApolloLink, HttpLink } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+
+const httpLink = new HttpLink({
+  uri: 'https://grants-stack-indexer-v2.gitcoin.co/graphql',
+});
+
+const logLink = new ApolloLink((operation, forward) => {
+  console.log(`GraphQL Request: ${operation.operationName}`);
+  console.log('Variables:', operation.variables);
+  console.log('Query:', operation.query.loc?.source.body);
+
+  return forward(operation).map((response) => {
+    console.log(`GraphQL Response: ${operation.operationName}`);
+    console.log('Response:', response);
+    return response;
+  });
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.error(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
+  }
+
+  if (networkError) {
+    console.error(`[Network error]: ${networkError}`);
+  }
+});
 
 const client = new ApolloClient({
-  uri: 'https://grants-stack-indexer-v2.gitcoin.co/graphql',
+  link: ApolloLink.from([errorLink, logLink, httpLink]),
   cache: new InMemoryCache(),
 });
 
 const FETCH_ROUND_BY_NODE_ID = gql`
-  query MyQuery {
-    roundByNodeId(nodeId: "WyJyb3VuZHMiLCIweDAwZDVlMGQzMWQzN2NjMTNjNjQ1ZDg2NDEwYWI0Y2I3Y2I0MjhjY2EiLDQyMTYxXQ==") {
+  query MyQuery($nodeId: ID!) {
+    roundByNodeId(nodeId: $nodeId) {
       id
       applications {
         chainId
@@ -67,11 +100,19 @@ export interface RoundByNodeIdResponse {
 
 export const fetchRoundByNodeId = async (
   nodeId: string,
-) => {
-  const { data } = await client.query({
+): Promise<RoundByNodeIdResponse> => {
+  console.log('Fetching data for nodeId:', nodeId); // Log the nodeId
+  const { data, errors } = await client.query<RoundByNodeIdResponse>({
     query: FETCH_ROUND_BY_NODE_ID,
     variables: { nodeId },
   });
+
+  console.log('GraphQL query:', FETCH_ROUND_BY_NODE_ID.loc?.source.body); // Log the query
+  console.log('Variables:', { nodeId }); // Log the variables
+  if (errors) {
+    console.error('GraphQL errors:', errors); // Log any errors
+  }
+  console.log('GraphQL response data:', data); // Log the response data
 
   return data;
 };
