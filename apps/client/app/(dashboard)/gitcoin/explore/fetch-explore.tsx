6a67b110581585f214bdb081298fcf3d30dcbe2f
Aryan Tiwari <aryantiwari7762@gmail.com>
2024-06-10 14:51:12 +0300

    randomised the gitcoin card

--- apps/client/app/(dashboard)/gitcoin/explore/fetch-explore.tsx
+++ apps/client/app/(dashboard)/gitcoin/explore/fetch-explore.tsx
@@ -1,11 +1,37 @@
 import GitcoinCard from '@/components/gitcoin/gitcoin-card';
 import { fetchRoundByNodeId } from '@/lib/actions';
 
+interface Application {
+  nodeId: string;
+  metadata: {
+    application: {
+      project: {
+        bannerImg: string;
+        description: string;
+      };
+    };
+  };
+  project: {
+    name: string;
+  };
+  totalAmountDonatedInUsd: number;
+  uniqueDonorsCount: number;
+}
+
+function shuffleArray<T>(array: T[]): T[] {
+  const arrayCopy = [...array];
+  return arrayCopy.sort(() => Math.random() - 0.5);
+}
+
 export default async function FetchGitcoins() {
   const applicationsInRound = await fetchRoundByNodeId('WyJyb3VuZHMiLCI2MyIsNDIxNjFd');
+  const shuffledApplications: Application[] = shuffleArray(
+    applicationsInRound.roundByNodeId.applications,
+  );
+
   return (
     <>
-      {applicationsInRound.roundByNodeId.applications.map((application) => (
+      {shuffledApplications.map((application: Application) => (
         <GitcoinCard
           id={application.nodeId}
           key={application.nodeId}
@@ -14,7 +40,8 @@ export default async function FetchGitcoins() {
           description={application.metadata.application.project.description}
           raised={application.totalAmountDonatedInUsd}
           contributors={application.uniqueDonorsCount}
-          link={`/gitcoin/${application.nodeId}`} />
+          link={`/gitcoin/${application.nodeId}`}
+        />
       ))}
     </>
   );
