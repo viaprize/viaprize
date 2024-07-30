6c0a82b5ffcdcafc6d0dd2eeb2bf99ea0bcedf30
dipanshuhappy <dipanshuhappy@gmail.com>
2024-07-30 17:06:20 +0530

    chore: Refactor PayPal checkout request and update PayPal URL in SummaryCard component

--- apps/client/app/(dashboard)/gitcoin/cart/summary-card.tsx
+++ apps/client/app/(dashboard)/gitcoin/cart/summary-card.tsx
@@ -1,165 +1,169 @@
 'use client';
-import { FUND_MCR_ADDRESS, USDC } from '@/lib/constants';
-import { encodedQFAllocation, usdcSignType } from '@/lib/utils';
-import { getTokenByChainIdAndAddress } from '@gitcoin/gitcoin-chain-data';
-import { Button, Card, Divider, Text } from '@mantine/core';
+import { Card, Divider, Text } from '@mantine/core';
 import { PayPalButtons, PayPalScriptProvider } from '@paypal/react-paypal-js';
-import { readContract } from '@wagmi/core';
 import { useCartStore } from 'app/(dashboard)/(_utils)/store/datastore';
 import { nanoid } from 'nanoid';
 import { useRouter } from 'next/navigation';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { toast } from 'sonner';
-import { encodeFunctionData, hashTypedData, Hex, hexToSignature, parseUnits } from 'viem';
 import { useWalletClient } from 'wagmi';
 export default function SummaryCard() {
   const [rawTxData, setRawTxData] = useState('');
-  const [customerId, setCustomerId] = useState<string>();
+  const [customerId, setCustomerId] = useState<string>(nanoid());
   const { data: walletClient } = useWalletClient();
   const router = useRouter();
-  const { items } = useCartStore();
-  const totalAmount = useMemo(
-    () => items.reduce((acc, item) => acc + parseFloat(item.amount), 0),
-    [items],
-  );
+  const { items, clearCart } = useCartStore();
+  const totalAmount = items.reduce((acc, item) => acc + parseFloat(item.amount), 0);
   useEffect(() => {
     setRawTxData('');
+    setCustomerId(nanoid());
   }, [totalAmount]);
-  const signDonations = async () => {
-    if (!walletClient) {
-      throw new Error('Wallet client not found');
-    }
-    const walletAddress = walletClient.account.address;
-    const projectsByChain = {
-      [8453]: items,
-    };
+  // const signDonations = async () => {
+  //   if (!walletClient) {
+  //     throw new Error('Wallet client not found');
+  //   }
+  //   const walletAddress = walletClient.account.address;
+  //   const shortenCartItems = items.map((item) => ({
+  //     amount: item.amount,
+  //     anchorAddress: item.anchorAddress,
+  //     roundId: item.roundId,
+  //   }));
 
-    const mcqAddress = '0x7C24f3494CC958CF268a92b45D7e54310d161794';
-    const usdcToken = getTokenByChainIdAndAddress(8453, USDC);
-    const deadline = Math.floor(Date.now() / 1000) + 100_000;
-    const totalDonationInUSDCOnChain = totalAmount * 1_000_000;
+  //   setRawTxData(JSON.stringify(shortenCartItems));
 
-    const chainId = 8453;
-    const donations = projectsByChain[chainId];
-    const nonce = await readContract({
-      abi: [
-        {
-          constant: true,
-          inputs: [
-            {
-              name: 'owner',
-              type: 'address',
-            },
-          ],
-          name: 'nonces',
-          outputs: [
-            {
-              name: '',
-              type: 'uint256',
-            },
-          ],
-          payable: false,
-          stateMutability: 'view',
-          type: 'function',
-        },
-      ] as const,
-      address: USDC,
-      functionName: 'nonces',
-      args: [walletAddress],
-    });
-    const hash = hashTypedData(
-      usdcSignType({
-        deadline: BigInt(deadline),
-        nonce: nonce,
-        owner: walletAddress,
-        spender: mcqAddress,
-        value: BigInt(totalDonationInUSDCOnChain),
-      }) as any,
-    );
-    const signature = await walletClient.signTypedData(
-      usdcSignType({
-        deadline: BigInt(deadline),
-        nonce: nonce,
-        owner: walletAddress,
-        spender: mcqAddress,
-        value: BigInt(totalDonationInUSDCOnChain),
-      }) as any,
-    );
-    const rsv = hexToSignature(signature);
+  //   const projectsByChain = {
+  //     [8453]: items,
+  //   };
 
-    const groupedDonationsByRoundId = Object.groupBy(
-      donations.map((d) => ({
-        ...d,
-        roundId: d.roundId,
-      })),
-      ({ roundId }) => roundId,
-    );
-    const groupedEncodedVotes: Record<string, Hex[]> = {};
-    if (!groupedDonationsByRoundId) {
-      throw new Error('groupedDonationsByRoundId is null');
-    }
-    for (const roundId in groupedDonationsByRoundId) {
-      if (!groupedDonationsByRoundId[roundId]) {
-        throw new Error('groupedDonationsByRoundId[roundId] is null');
-      }
-      groupedEncodedVotes[roundId] = encodedQFAllocation(
-        usdcToken,
-        // @ts-ignore: Object is possibly 'null'.
-        groupedDonationsByRoundId![roundId].map((d) => ({
-          anchorAddress: d.anchorAddress ?? '',
-          amount: d.amount,
-        })),
-      );
-    }
-    // const groupedAmounts: Record<string, bigint> = {};
-    // for (const roundId in groupedDonationsByRoundId) {
-    //   if (!groupedDonationsByRoundId[roundId]) {
-    //     continue;
-    //   }
-    //   groupedAmounts[roundId] = groupedDonationsByRoundId[roundId].reduce(
-    //     (acc, donation) => acc + parseUnits(donation.amount, usdcToken.decimals),
-    //     BigInt(0),
-    //   );
-    // }
-    const amountArray: bigint[] = [];
-    for (const roundId in groupedDonationsByRoundId) {
-      if (!groupedDonationsByRoundId[roundId]) {
-        continue;
-      }
-      // @ts-ignore: Object is possibly 'null'.
-      groupedDonationsByRoundId[roundId].map((donation) => {
-        amountArray.push(parseUnits(donation.amount, usdcToken.decimals));
-      });
-    }
-    const poolIds = Object.keys(groupedEncodedVotes).flatMap((key) => {
-      const count = groupedEncodedVotes[key].length;
-      return new Array(count).fill(key);
-    });
-    const data = Object.values(groupedEncodedVotes).flat();
+  //   const mcqAddress = '0x7C24f3494CC958CF268a92b45D7e54310d161794';
+  //   const usdcToken = getTokenByChainIdAndAddress(8453, USDC);
+  //   const deadline = Math.floor(Date.now() / 1000) + 100_000;
+  //   const totalDonationInUSDCOnChain = totalAmount * 1_000_000;
 
-    const newRawTxData = encodeFunctionData({
-      abi: FUND_MCR_ADDRESS,
-      functionName: 'fundUsingUsdc',
-      args: [
-        '0xF7D1D901d15BBf60a8e896fbA7BBD4AB4C1021b3',
-        mcqAddress,
-        hash,
-        data,
-        poolIds,
-        Object.values(amountArray),
-        Object.values(amountArray).reduce((acc, b) => acc + b),
-        usdcToken.address as Hex,
-        BigInt(deadline),
-        parseInt(rsv.v.toString()),
-        rsv.r as Hex,
-        rsv.s as Hex,
-      ],
-    });
-    setRawTxData(newRawTxData);
-  };
+  //   const chainId = 8453;
+  //   const donations = projectsByChain[chainId];
+  //   const nonce = await readContract({
+  //     abi: [
+  //       {
+  //         constant: true,
+  //         inputs: [
+  //           {
+  //             name: 'owner',
+  //             type: 'address',
+  //           },
+  //         ],
+  //         name: 'nonces',
+  //         outputs: [
+  //           {
+  //             name: '',
+  //             type: 'uint256',
+  //           },
+  //         ],
+  //         payable: false,
+  //         stateMutability: 'view',
+  //         type: 'function',
+  //       },
+  //     ] as const,
+  //     address: USDC,
+  //     functionName: 'nonces',
+  //     args: [walletAddress],
+  //   });
+  //   const hash = hashTypedData(
+  //     usdcSignType({
+  //       deadline: BigInt(deadline),
+  //       nonce: nonce,
+  //       owner: walletAddress,
+  //       spender: mcqAddress,
+  //       value: BigInt(totalDonationInUSDCOnChain),
+  //     }) as any,
+  //   );
+  //   const signature = await walletClient.signTypedData(
+  //     usdcSignType({
+  //       deadline: BigInt(deadline),
+  //       nonce: nonce,
+  //       owner: walletAddress,
+  //       spender: mcqAddress,
+  //       value: BigInt(totalDonationInUSDCOnChain),
+  //     }) as any,
+  //   );
+  //   const rsv = hexToSignature(signature);
+
+  //   const groupedDonationsByRoundId = Object.groupBy(
+  //     donations.map((d) => ({
+  //       amount: d.amount,
+  //       anchorAddress: d.anchorAddress,
+  //       roundId: d.roundId,
+  //     })),
+  //     ({ roundId }) => roundId,
+  //   );
+  //   const groupedEncodedVotes: Record<string, Hex[]> = {};
+  //   if (!groupedDonationsByRoundId) {
+  //     throw new Error('groupedDonationsByRoundId is null');
+  //   }
+  //   for (const roundId in groupedDonationsByRoundId) {
+  //     if (!groupedDonationsByRoundId[roundId]) {
+  //       throw new Error('groupedDonationsByRoundId[roundId] is null');
+  //     }
+  //     groupedEncodedVotes[roundId] = encodedQFAllocation(
+  //       usdcToken,
+  //       // @ts-ignore: Object is possibly 'null'.
+  //       groupedDonationsByRoundId![roundId].map((d) => ({
+  //         anchorAddress: d.anchorAddress ?? '',
+  //         amount: d.amount,
+  //       })),
+  //     );
+  //   }
+  //   // const groupedAmounts: Record<string, bigint> = {};
+  //   // for (const roundId in groupedDonationsByRoundId) {
+  //   //   if (!groupedDonationsByRoundId[roundId]) {
+  //   //     continue;
+  //   //   }
+  //   //   groupedAmounts[roundId] = groupedDonationsByRoundId[roundId].reduce(
+  //   //     (acc, donation) => acc + parseUnits(donation.amount, usdcToken.decimals),
+  //   //     BigInt(0),
+  //   //   );
+  //   // }
+  //     const amountArray: bigint[] = [];
+  //     for (const roundId in groupedDonationsByRoundId) {
+  //       if (!groupedDonationsByRoundId[roundId]) {
+  //         continue;
+  //       }
+  //       // @ts-ignore: Object is possibly 'null'.
+  //       groupedDonationsByRoundId[roundId].map((donation) => {
+  //         amountArray.push(parseUnits(donation.amount, usdcToken.decimals));
+  //       });
+  //     }
+  //     const poolIds = Object.keys(groupedEncodedVotes).flatMap((key) => {
+  //       const count = groupedEncodedVotes[key].length;
+  //       return new Array(count).fill(key);
+  //     });
+  //     const data = Object.values(groupedEncodedVotes).flat();
+
+  //     const newRawTxData = encodeFunctionData({
+  //       abi: MULTI_ROUND_CHECKOUT,
+  //       functionName: 'allocateERC20Permit',
+  //       args: [
+  //         data,
+  //         poolIds,
+  //         Object.values(amountArray),
+  //         Object.values(amountArray).reduce((acc, b) => acc + b),
+  //         usdcToken.address as Hex,
+  //         BigInt(deadline),
+  //         parseInt(rsv.v.toString()),
+  //         rsv.r as Hex,
+  //         rsv.s as Hex,
+  //       ],
+  //     });
+  //   };
+  // };
   const sumbit = async () => {
     try {
-      const customerId = nanoid();
+      const shortenCartItems = items.map((item) => ({
+        amount: item.amount,
+        anchorAddress: item.anchorAddress,
+        roundId: item.roundId,
+      }));
+      console.log({ shortenCartItems });
 
       const checkoutUrl = await fetch(
         'https://fxk2d1d3nf.execute-api.us-west-1.amazonaws.com/checkout/paypal',
@@ -169,7 +173,7 @@ export default function SummaryCard() {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify({
-            metadata: rawTxData,
+            metadata: JSON.stringify(shortenCartItems),
             customId: customerId,
             amount: totalAmount,
           }),
@@ -179,7 +183,6 @@ export default function SummaryCard() {
         .then((data) => data);
 
       console.log({ checkoutUrl });
-      setCustomerId(customerId);
 
       console.log({ customerId });
 
@@ -208,45 +211,49 @@ export default function SummaryCard() {
         </Text>
       </div>
       <Divider />
-      {rawTxData.length === 0 ? (
-        <Button onClick={signDonations}>Sign With Wallet</Button>
-      ) : (
-        <PayPalScriptProvider
-          options={{
-            clientId:
-              'ARWRaruLPRFS3ekuyixocUzPBxKUEacRHjzVR5HP-1lLJS-Fj0BJkHZ_CmA-OlQsicXGenwgOqMnYAqs',
-          }}
-        >
-          <PayPalButtons
-            style={{ layout: 'horizontal' }}
-            createOrder={async () => {
-              const id = await sumbit();
-              if (!id) {
-                throw new Error('Checkout ID not found');
-              }
-              return id;
-            }}
-            onApprove={(data, actions) => {
-              return fetch(
-                'https://fxk2d1d3nf.execute-api.us-west-1.amazonaws.com/checkout/paypal/capture',
-                {
-                  method: 'POST',
 
-                  body: JSON.stringify({
-                    orderId: data.orderID,
-                    customId: customerId,
-                  }),
-                },
-              )
-                .then((response) => response.json())
-                .then((orderData) => {
-                  const name = orderData.payer.name.given_name;
-                  alert(`Transaction completed by ${name}`);
-                });
-            }}
-          />
-        </PayPalScriptProvider>
-      )}
+      <PayPalScriptProvider
+        options={{
+          clientId:
+            'ARWRaruLPRFS3ekuyixocUzPBxKUEacRHjzVR5HP-1lLJS-Fj0BJkHZ_CmA-OlQsicXGenwgOqMnYAqs',
+        }}
+      >
+        <PayPalButtons
+          style={{ layout: 'vertical' }}
+          createOrder={async () => {
+            const id = await sumbit();
+            if (!id) {
+              throw new Error('Checkout ID not found');
+            }
+            return id;
+          }}
+          onApprove={(data, actions) => {
+            console.log({ customerId });
+            console.log({ data });
+            return fetch(
+              'https://fxk2d1d3nf.execute-api.us-west-1.amazonaws.com/checkout/paypal/capture',
+              {
+                method: 'POST',
+
+                body: JSON.stringify({
+                  orderId: data.orderID,
+                  customId: customerId,
+                }),
+              },
+            )
+              .then((response) => response.json())
+              .then((orderData) => {
+                toast.success(
+                  'Transaction completed by ' + orderData.payer.name.given_name,
+                );
+                clearCart();
+
+                const name = orderData.payer.name.given_name;
+                alert(`Transaction completed by ${name}`);
+              });
+          }}
+        />
+      </PayPalScriptProvider>
     </Card>
   );
 }
--- apps/client/lib/constants.ts
+++ apps/client/lib/constants.ts
@@ -305,3 +305,483 @@ export const FUND_MCR_ADDRESS = [
     type: 'function',
   },
 ] as const;
+
+export const MULTI_ROUND_CHECKOUT = [
+  {
+    inputs: [],
+    name: 'AmountsNotEqualRoundsLength',
+    type: 'error',
+  },
+  {
+    inputs: [],
+    name: 'ExcessAmountSent',
+    type: 'error',
+  },
+  {
+    inputs: [],
+    name: 'INVALID_INPUT',
+    type: 'error',
+  },
+  {
+    inputs: [],
+    name: 'InvalidInitialization',
+    type: 'error',
+  },
+  {
+    inputs: [],
+    name: 'NotInitializing',
+    type: 'error',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
+      },
+    ],
+    name: 'OwnableInvalidOwner',
+    type: 'error',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'account',
+        type: 'address',
+      },
+    ],
+    name: 'OwnableUnauthorizedAccount',
+    type: 'error',
+  },
+  {
+    inputs: [],
+    name: 'VotesNotEqualRoundsLength',
+    type: 'error',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: 'uint8',
+        name: 'version',
+        type: 'uint8',
+      },
+    ],
+    name: 'Initialized',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'previousOwner',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'newOwner',
+        type: 'address',
+      },
+    ],
+    name: 'OwnershipTransferred',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: 'address',
+        name: 'account',
+        type: 'address',
+      },
+    ],
+    name: 'Paused',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: 'address',
+        name: 'account',
+        type: 'address',
+      },
+    ],
+    name: 'Unpaused',
+    type: 'event',
+  },
+  {
+    inputs: [],
+    name: 'allo',
+    outputs: [
+      {
+        internalType: 'contract IAllo',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256[]',
+        name: '_poolIds',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'uint256[]',
+        name: '_amounts',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'bytes[]',
+        name: '_data',
+        type: 'bytes[]',
+      },
+    ],
+    name: 'allocate',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'bytes[]',
+        name: '_data',
+        type: 'bytes[]',
+      },
+      {
+        internalType: 'uint256[]',
+        name: '_poolIds',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'uint256[]',
+        name: '_amounts',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'uint256',
+        name: 'totalAmount',
+        type: 'uint256',
+      },
+      {
+        internalType: 'address',
+        name: 'token',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'deadline',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'nonce',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint8',
+        name: 'v',
+        type: 'uint8',
+      },
+      {
+        internalType: 'bytes32',
+        name: 'r',
+        type: 'bytes32',
+      },
+      {
+        internalType: 'bytes32',
+        name: 's',
+        type: 'bytes32',
+      },
+    ],
+    name: 'allocateDAIPermit',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'bytes[]',
+        name: '_data',
+        type: 'bytes[]',
+      },
+      {
+        internalType: 'uint256[]',
+        name: '_poolIds',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'uint256[]',
+        name: '_amounts',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'uint256',
+        name: 'totalAmount',
+        type: 'uint256',
+      },
+      {
+        internalType: 'address',
+        name: 'token',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'deadline',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint8',
+        name: 'v',
+        type: 'uint8',
+      },
+      {
+        internalType: 'bytes32',
+        name: 'r',
+        type: 'bytes32',
+      },
+      {
+        internalType: 'bytes32',
+        name: 's',
+        type: 'bytes32',
+      },
+    ],
+    name: 'allocateERC20Permit',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '_allo',
+        type: 'address',
+      },
+    ],
+    name: 'initialize',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'owner',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'pause',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'paused',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'renounceOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'newOwner',
+        type: 'address',
+      },
+    ],
+    name: 'transferOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'unpause',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '_allo',
+        type: 'address',
+      },
+    ],
+    name: 'updateAllo',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'bytes[][]',
+        name: 'votes',
+        type: 'bytes[][]',
+      },
+      {
+        internalType: 'address[]',
+        name: 'rounds',
+        type: 'address[]',
+      },
+      {
+        internalType: 'uint256[]',
+        name: 'amounts',
+        type: 'uint256[]',
+      },
+    ],
+    name: 'vote',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'bytes[][]',
+        name: 'votes',
+        type: 'bytes[][]',
+      },
+      {
+        internalType: 'address[]',
+        name: 'rounds',
+        type: 'address[]',
+      },
+      {
+        internalType: 'uint256[]',
+        name: 'amounts',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'uint256',
+        name: 'totalAmount',
+        type: 'uint256',
+      },
+      {
+        internalType: 'address',
+        name: 'token',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'deadline',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'nonce',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint8',
+        name: 'v',
+        type: 'uint8',
+      },
+      {
+        internalType: 'bytes32',
+        name: 'r',
+        type: 'bytes32',
+      },
+      {
+        internalType: 'bytes32',
+        name: 's',
+        type: 'bytes32',
+      },
+    ],
+    name: 'voteDAIPermit',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'bytes[][]',
+        name: 'votes',
+        type: 'bytes[][]',
+      },
+      {
+        internalType: 'address[]',
+        name: 'rounds',
+        type: 'address[]',
+      },
+      {
+        internalType: 'uint256[]',
+        name: 'amounts',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'uint256',
+        name: 'totalAmount',
+        type: 'uint256',
+      },
+      {
+        internalType: 'address',
+        name: 'token',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'deadline',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint8',
+        name: 'v',
+        type: 'uint8',
+      },
+      {
+        internalType: 'bytes32',
+        name: 'r',
+        type: 'bytes32',
+      },
+      {
+        internalType: 'bytes32',
+        name: 's',
+        type: 'bytes32',
+      },
+    ],
+    name: 'voteERC20Permit',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+] as const;
