178b1a5db33a35345017510300091da5edd39b02
Aryan Tiwari <aryantiwari7762@gmail.com>
2024-08-13 22:37:07 +0530

    email exiist text

--- apps/client/app/(onboarding)/onboarding/page.tsx
+++ apps/client/app/(onboarding)/onboarding/page.tsx
@@ -18,9 +18,11 @@ import { useDebouncedValue } from '@mantine/hooks';
 import { usePrivy } from '@privy-io/react-auth';
 import { useRouter } from 'next/navigation';
 import type { ChangeEvent } from 'react';
-import { useCallback, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useMutation } from 'react-query';
 import { toast } from 'sonner';
+import { IconLock } from '@tabler/icons-react';
+
 
 export default function Details() {
   const timeoutRef = useRef<number>(-1);
@@ -31,6 +33,9 @@ export default function Details() {
   const [name, setName] = useState('');
   const [username, setUsername] = useState('');
   const [bio, setBio] = useState('');
+  const [emailLocked, setEmailLocked] = useState(Boolean(user?.email?.address));
+  const [emailExists, setEmailExists] = useState(false);
+  const [emailLoading, setEmailLoading] = useState(false);
 
   const { createNewUser, getLastVisitedPage } = useAppUser();
 
@@ -44,25 +49,6 @@ export default function Details() {
 
   const router = useRouter();
   const { logoutUser } = useAppUser();
-  const handleChange = (val: string) => {
-    window.clearTimeout(timeoutRef.current);
-    setEmail(val);
-    setData([]);
-
-    if (val.trim().length === 0 || val.includes('@')) {
-      setLoading(false);
-    } else {
-      setLoading(true);
-      timeoutRef.current = window.setTimeout(() => {
-        setLoading(false);
-        setData(
-          ['gmail.com', 'outlook.com', 'yahoo.com'].map(
-            (provider) => `${val}@${provider}`,
-          ),
-        );
-      }, 1000);
-    }
-  };
 
   const handleLogin = () => {
     try {
@@ -86,6 +72,7 @@ export default function Details() {
   };
 
   const [debouncedUsername] = useDebouncedValue(username, 200);
+  const [debouncedEmail] = useDebouncedValue(email, 200);
 
   const [exists, setExists] = useState(false);
   const [usernameLoading, setUsernameLoading] = useState(false);
@@ -101,21 +88,41 @@ export default function Details() {
         setLogoutLoading(false);
       });
   };
+
   const checkUsername = useCallback(
     async (newusername: string) => {
       try {
         console.log('checking newusername', newusername);
         const response = await new Api().users.existsDetail(newusername);
         setExists(response.data);
+        console.log('response', response.data);
       } catch (error) {
         console.error(error);
       } finally {
         setUsernameLoading(false);
       }
     },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     [debouncedUsername],
   );
+
+  const checkEmail = useCallback(
+    async () => {
+      try {
+        console.log('checking newEmail', debouncedEmail);
+        const response = await new Api().users.existsEmailDetail(debouncedEmail);
+        setEmailExists(response.data);
+        console.log('response', response.data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setEmailLoading(false);
+      }
+    },
+    [debouncedEmail],
+  );
+   useEffect(() => {
+     checkEmail().catch(console.error);
+   }, [debouncedEmail]);
 
   const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
     const value = event.currentTarget.value.toLowerCase().replace(/\s/g, '');
@@ -126,6 +133,29 @@ export default function Details() {
         checkUsername(value).catch(console.error);
       })
       .catch(console.error);
+  };
+
+  const handleChange = (val: string) => {
+    window.clearTimeout(timeoutRef.current);
+    setEmail(val);
+    setData([]);
+
+    if (val.trim().length === 0 || val.includes('@')) {
+      setLoading(false);
+      setEmailLoading(false);
+    } else {
+      setLoading(true);
+      setEmailLoading(true);
+      timeoutRef.current = window.setTimeout(() => {
+        setLoading(false);
+        setData(
+          ['gmail.com', 'outlook.com', 'yahoo.com'].map(
+            (provider) => `${val}@${provider}`,
+          ),
+        );
+        // checkEmail().catch(console.error);
+      }, 1000);
+    }
   };
 
   return (
@@ -168,19 +198,30 @@ export default function Details() {
           placeholder="Enter your email"
           data={data}
           my="sm"
+          disabled={emailLocked}
+          leftSection={emailLocked ? <IconLock size={16} /> : null}
         />
-        {/* <Textarea
-          size="lg"
-          radius="xs"
-          label="User Bio"
-          description="Type in something about yourself"
-          value={bio}
-          onChange={(e) => setBio(e.currentTarget.value)}
-        /> */}
+        <Flex>
+          {emailLoading ? <Loader m="xs" size="xs" /> : null}
+          {!emailLoading &&
+            debouncedEmail.length > 0 &&
+            (emailExists && debouncedEmail.length > 0 ? (
+              <Text c="red">Email already exists </Text>
+            ) : (
+              <Text c="green">{email} Is a Valid Email </Text>
+            ))}
+        </Flex>
         <Button
           onClick={handleLogin}
           loading={loading || uploadUserMutation.isLoading}
-          disabled={loading || uploadUserMutation.isLoading || exists || usernameLoading}
+          disabled={
+            loading ||
+            uploadUserMutation.isLoading ||
+            exists ||
+            emailExists ||
+            usernameLoading ||
+            emailLoading
+          }
           color="blue"
           fullWidth
           my="sm"
@@ -190,7 +231,14 @@ export default function Details() {
         <Button
           onClick={logout}
           loading={logoutLoading}
-          disabled={loading || uploadUserMutation.isLoading || exists || usernameLoading}
+          disabled={
+            loading ||
+            uploadUserMutation.isLoading ||
+            exists ||
+            emailExists ||
+            usernameLoading ||
+            emailLoading
+          }
           color="blue"
           fullWidth
           my="sm"
