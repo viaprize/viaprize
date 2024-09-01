37829304db1ea007722a2a65bc4140a24ec1439c
Swaraj Bachu <rajswaraj.r@gmail.com>
2024-09-01 21:54:31 +0530

    responsiveness fix

--- packages/website/src/app/(dashboard)/layout.tsx
+++ packages/website/src/app/(dashboard)/layout.tsx
@@ -8,21 +8,21 @@ export default function DashBoardlayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="h-[100vh] w-full overflow-hidden bg-slate-100 dark:bg-zinc-900">
+    <div className=" h-dvh w-full overflow-hidden bg-slate-100 dark:bg-zinc-900">
       {/* Header at the top */}
-      <header className="sticky top-0 z-50 mt-2 mx-2">
+      {/* <header className="sticky top-0 z-50 mt-2 mx-2">
         <Header />
-      </header>
+      </header> */}
 
       {/* Main content area with sidebar and children */}
-      <div className="flex h-full">
+      <div className="flex md:flex-row flex-col h-full">
         {/* Sidebar on the left */}
-        <nav className="flex-shrink-0 m-2 ">
+        {/* <nav className="flex-shrink-0 m-2 "> */}
           <SideNavbarConfigure />
-        </nav>
+        {/* </nav> */}
 
         {/* Children content on the right */}
-        <main className="h-full w-full flex-1 overflow-auto bg-background  rounded-md mr-2 my-2">
+        <main className="h-full w-full flex-1 overflow-auto bg-background  rounded-md md:mr-2 md:my-2">
           {children}
         </main>
       </div>
--- packages/website/src/app/(dashboard)/prize/page.tsx
+++ packages/website/src/app/(dashboard)/prize/page.tsx
@@ -3,14 +3,14 @@ import FetchExploreCard from '@/components/prize/explore/fetch-explore-card'
 export default function ExplorePage() {
   return (
     <div className="flex h-full">
-      <div className="w-[75%] h-full border-r-2">
+      <div className="w-full md:w-[75%] h-full border-r-2">
         {/* <div className="mt-2 w-[35%] ml-3">
           <OverallPrizeStatus />
         </div>
         <Separator className="mb-7 mt-4 w-full" /> */}
         <FetchExploreCard />
       </div>
-      <div className="w-[25%] mt-5 mx-2">wdufiuew</div>
+      <div className="w-[25%] mt-5 mx-2 md:block hidden">wdufiuew</div>
     </div>
   )
 }
