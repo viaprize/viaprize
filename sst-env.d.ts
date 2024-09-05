aaf38d09935d1c881f07d2d22187c364c391968b
dipanshuhappy <dipanshuhappy@gmail.com>
2024-09-05 18:47:08 +0530

    Refactor authentication code and update dependencies

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -1,60 +1,52 @@
 /* tslint:disable */
 /* eslint-disable */
-import 'sst'
-declare module 'sst' {
+import "sst";
+declare module "sst" {
   export interface Resource {
     AuthGithubId: {
-      type: 'sst.sst.Secret'
-      value: string
-    }
+      type: "sst.sst.Secret";
+      value: string;
+    };
     AuthGithubSecret: {
-      type: 'sst.sst.Secret'
-      value: string
-    }
+      type: "sst.sst.Secret";
+      value: string;
+    };
     AuthGoogleId: {
-      type: 'sst.sst.Secret'
-      value: string
-    }
+      type: "sst.sst.Secret";
+      value: string;
+    };
     AuthGoogleSecret: {
-      type: 'sst.sst.Secret'
-      value: string
-    }
+      type: "sst.sst.Secret";
+      value: string;
+    };
     AuthResendKey: {
-      type: 'sst.sst.Secret'
-      value: string
-    }
+      type: "sst.sst.Secret";
+      value: string;
+    };
     AuthSecret: {
-      type: 'sst.sst.Secret'
-      value: string
-    }
-
-    DatabaseUrl: {
-      type: 'sst.sst.Secret'
-      value: string
-    }
-    MyApi: {
-      name: string
-      type: 'sst.aws.Function'
-      url: string
-    }
-
+      type: "sst.sst.Secret";
+      value: string;
+    };
     AuthTrustHost: {
-      type: 'sst.sst.Secret'
-      value: string
-    }
-
+      type: "sst.sst.Secret";
+      value: string;
+    };
+    DatabaseUrl: {
+      type: "sst.sst.Secret";
+      value: string;
+    };
     NextPublicWalletConnectProjectId: {
-      type: 'sst.sst.Secret'
-      value: string
-    }
+      type: "sst.sst.Secret";
+      value: string;
+    };
     WalletPaymentInfraApi: {
-      type: 'sst.sst.Secret'
-      value: string
-    }
-
+      type: "sst.sst.Secret";
+      value: string;
+    };
     website: {
-      type: 'sst.aws.Nextjs'
-      url: string
-    }
+      type: "sst.aws.Nextjs";
+      url: string;
+    };
   }
 }
+export {};
