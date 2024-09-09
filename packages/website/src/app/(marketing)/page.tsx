5cfdfc97b41d2effba834f0d2d6616f6001b0687
Swaraj Bachu <rajswaraj.r@gmail.com>
2024-09-09 11:08:28 +0530

    trying something

---
+++ packages/website/src/app/(marketing)/page.tsx
@@ -0,0 +1,9 @@
+import HeroSection from "@/components/home/hero";
+
+export default async function Home() {
+  return (
+    <main className="flex flex-col ">
+      <HeroSection />
+    </main>
+  );
+}
