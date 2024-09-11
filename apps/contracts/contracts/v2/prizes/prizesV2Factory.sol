4040e0b22c0fc7b8ae3f6a17b9c5eacb9fa42b71
Dipanshu Singh <58115782+dipanshuhappy@users.noreply.github.com>
2024-09-11 15:19:09 +0530

    Update prizesV2Factory.sol

--- apps/contracts/contracts/v2/prizes/prizesV2Factory.sol
+++ apps/contracts/contracts/v2/prizes/prizesV2Factory.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.19;
 import "./prizes.sol";
 contract PrizeFactoryV2 {
     // Event declaration
-    event NewViaPrizeCreated(uint indexed id, address indexed viaPrizeAddress);	   
+    event NewViaPrizeCreated(string indexed id, address indexed viaPrizeAddress);	   
     function createViaPrize(
-       uint _id , address _proposer, address[] memory _platformAdmins, uint _platFormFee, uint _proposerFee, address _usdcAddress, address _usdcBridgedAddress , address _swapRouter ,address _usdcToUsdcePool,address _usdcToEthPool,address _ethPriceAggregator,address _wethToken
+       string memory _id , address _proposer, address[] memory _platformAdmins, uint8 _platFormFee, uint8 _proposerFee, address _usdcAddress, address _usdcBridgedAddress , address _swapRouter ,address _usdcToUsdcePool,address _usdcToEthPool,address _ethPriceAggregator,address _wethToken
     ) public returns (address) {
         // Deploy a new ViaPrize contract and store its address	        // Deploy a new ViaPrize contract and store its address
         PrizeV2 newViaPrize = new PrizeV2(_proposer, _platformAdmins, _platFormFee, _proposerFee, _usdcAddress,_usdcBridgedAddress,_swapRouter,_usdcToUsdcePool,_usdcToEthPool,_ethPriceAggregator,_wethToken);
