a2bf1a17afc5151280b03942882049772808ed77
dipanshuhappy <dipanshuhappy@gmail.com>
2024-09-13 02:23:56 +0530

    Refactor storage.ts to conditionally create imageBucket based on the app stage

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,3 +1,6 @@
-export const imageBucket = new sst.aws.Bucket("ImageUploads", {
-  public: true,
-});
+export const imageBucket =
+  $app.stage === "prod"
+    ? new sst.aws.Bucket("ImageUploads", {
+        public: true,
+      })
+    : sst.aws.Bucket.get("ImageUploads", "viaprize-dev-imageuploads-brehvnec");
