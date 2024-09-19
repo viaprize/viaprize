d4691409994d07d4998bd69f5526289b2df18818
Aryan Tiwari <aryantiwari7762@gmail.com>
2024-09-19 21:21:06 +0530

    details section

--- packages/website/src/app/(dashboard)/prize/[slug]/page.tsx
+++ packages/website/src/app/(dashboard)/prize/[slug]/page.tsx
@@ -1,18 +1,25 @@
+import AboutContent from '@/components/prize/details/about-content'
 import DetailHeader from '@/components/prize/details/details-header'
+import VisionaryFunderCard from '@/components/prize/details/visionary-funder-card'
 import { IconArrowLeft } from '@tabler/icons-react'
+import { Separator } from '@viaprize/ui/separator'
 
 export default function PrizeDetails() {
   return (
     <div className="flex h-full">
       <div className="w-full md:w-[75%] h-full border-r-2">
-        <div className="p-3 space-y-3">
-          <div className="flex items-center text-sm font-semibold">
+        <div className="space-y-3">
+          <div className="flex items-center text-sm font-semibold ml-3 mt-3">
             <IconArrowLeft className="mr-1" size={20} /> Back
           </div>
           <DetailHeader />
+          <Separator className='my-2'/>
+          <AboutContent />
         </div>
       </div>
-      <div className="w-[25%] mt-5 mx-2 md:block hidden">wdufiuew</div>
+      <div className="w-[25%] mt-5 mx-2 md:block hidden">
+        <VisionaryFunderCard />
+      </div>
     </div>
   )
 }
