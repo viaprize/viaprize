8653dec480d9bb850946df9b5d8e1d66d41b4987
Swaraj Bachu <rajswaraj.r@gmail.com>
2024-09-26 20:52:02 +0530

    format

--- packages/core/src/database/schema/users.ts
+++ packages/core/src/database/schema/users.ts
@@ -1,44 +1,38 @@
-import { relations } from "drizzle-orm";
-import {
-  boolean,
-  pgTable,
-  text,
-  timestamp,
-  varchar,
-} from "drizzle-orm/pg-core";
-import { createInsertSchema, createSelectSchema } from "drizzle-zod";
-import { nanoid } from "nanoid";
-import { prizes } from "./prizes";
-import { prizesToContestants } from "./prizes-to-contestants";
-import { submissions } from "./submissions";
-import { wallets } from "./wallets";
-import { z } from "zod";
+import { relations } from 'drizzle-orm'
+import { boolean, pgTable, text, timestamp, varchar } from 'drizzle-orm/pg-core'
+import { createInsertSchema, createSelectSchema } from 'drizzle-zod'
+import { nanoid } from 'nanoid'
+import type { z } from 'zod'
+import { prizes } from './prizes'
+import { prizesToContestants } from './prizes-to-contestants'
+import { submissions } from './submissions'
+import { wallets } from './wallets'
 
-export const users = pgTable("user", {
-  id: text("id")
+export const users = pgTable('user', {
+  id: text('id')
     .primaryKey()
     .$default(() => nanoid(12)),
-  email: text("email").unique().notNull(),
-  authId: varchar("authId").unique(),
-  username: varchar("username").unique(),
-  provider: varchar("provider"),
-  name: varchar("name"),
-  emailVerified: timestamp("emailVerified", { mode: "date" }),
-  image: text("image"),
-  isAdmin: boolean("isAdmin").default(false),
-  bio: text("bio").default(""),
-  avatar: text("avatar"),
-  skillSets: text("skillSets").array(),
-  priorities: text("priorities").array(),
-  updatedAt: timestamp("updatedAt", {
-    mode: "date",
+  email: text('email').unique().notNull(),
+  authId: varchar('authId').unique(),
+  username: varchar('username').unique(),
+  provider: varchar('provider'),
+  name: varchar('name'),
+  emailVerified: timestamp('emailVerified', { mode: 'date' }),
+  image: text('image'),
+  isAdmin: boolean('isAdmin').default(false),
+  bio: text('bio').default(''),
+  avatar: text('avatar'),
+  skillSets: text('skillSets').array(),
+  priorities: text('priorities').array(),
+  updatedAt: timestamp('updatedAt', {
+    mode: 'date',
     withTimezone: true,
   }).$onUpdate(() => new Date()),
-  createdAt: timestamp("createdAt", {
-    mode: "date",
+  createdAt: timestamp('createdAt', {
+    mode: 'date',
     withTimezone: true,
   }).$default(() => new Date()),
-});
+})
 
 export const usersRelations = relations(users, ({ many }) => ({
   wallets: many(wallets),
@@ -46,8 +40,8 @@ export const usersRelations = relations(users, ({ many }) => ({
   prizeComments: many(users),
   submission: many(submissions),
   contestants: many(prizesToContestants),
-}));
+}))
 
-export const insertUserSchema = createInsertSchema(users);
-export const selectUserSchema = createSelectSchema(users);
-export type selectUserType = z.input<typeof selectUserSchema>;
+export const insertUserSchema = createInsertSchema(users)
+export const selectUserSchema = createSelectSchema(users)
+export type selectUserType = z.input<typeof selectUserSchema>
