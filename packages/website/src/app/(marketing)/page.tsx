d40e512df128034d822f3f43f5c8df5e4327d3b9
dipanshuhappy <dipanshuhappy@gmail.com>
2025-04-17 14:35:00 -0400

    Redirect homepage to external URL and comment out previous content

--- packages/website/src/app/(marketing)/page.tsx
+++ packages/website/src/app/(marketing)/page.tsx
@@ -5,15 +5,17 @@ import IdeaMarket from '@/components/home/idea-market'
 import StepsForFunders from '@/components/home/steps'
 import Testimonials from '@/components/home/testimonials'
 import { env } from '@/env'
+import { redirect } from 'next/navigation'
 
 export default async function Home() {
-  return (
-    <main className="overflow-hidden">
-      <HeroSection />
-      <IdeaMarket />
-      {/* <StepsForFunders /> */}
-      <Testimonials />
-      <ContactSection />
-    </main>
-  )
+  return redirect('https://vprz.org/')
+  // return (
+  //   <main className="overflow-hidden">
+  //     <HeroSection />
+  //     <IdeaMarket />
+  //     {/* <StepsForFunders /> */}
+  //     <Testimonials />
+  //     <ContactSection />
+  //   </main>
+  // )
 }
